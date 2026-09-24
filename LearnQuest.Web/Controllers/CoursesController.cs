850e90b4349ad10a8a087bb21a579049fb0335d6
murat <murat.16-16@hotmail.com>
2026-09-24 20:38:03 +0300

    feat: add courses page UI and filter components

---
+++ LearnQuest.Web/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnQuest.Web.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

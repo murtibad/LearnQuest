e3d2059751c70d787f7bc78f90ee4e7a169744ac
murat <murat.16-16@hotmail.com>
2026-09-24 22:45:33 +0300

    feat: add interactive frontend quiz arena for student workflow

--- LearnQuest.Web/Controllers/DashboardController.cs
+++ LearnQuest.Web/Controllers/DashboardController.cs
@@ -4,6 +4,10 @@ namespace LearnQuest.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        public IActionResult Quiz()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();

fb76347d676a16a173efda326c592793315eca9e
murat <murat.16-16@hotmail.com>
2026-09-24 21:17:08 +0300

    fix: update landing hero image scaling and apply english comments

---
+++ LearnQuest.Web/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnQuest.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        // Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // Forgot Password
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}

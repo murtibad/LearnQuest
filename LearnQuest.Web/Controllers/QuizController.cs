c4ff6db2bc068fd94ebc36df4232ad2124053744
murat <murat.16-16@hotmail.com>
2026-09-24 20:46:43 +0300

    feat: add quiz page UI and dynamic active menu

---
+++ LearnQuest.Web/Controllers/QuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnQuest.Web.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
